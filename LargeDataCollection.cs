cf234294b4c57bbf371c6984ac5d35581a93e56a
Aishu030 <aishuramesh031@gmail.com>
2023-07-20 18:12:41 +0530

    firstcommit

---
+++ LargeDataCollection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+//Assignment11
+//IDISPOSABLE
+namespace Assignment11
+{
+    class LargeDataCollection : IDisposable
+    {
+        private List<string> dataCollection;
+
+        public LargeDataCollection(List<string> initialData)
+        {
+            dataCollection = new List<string>(initialData);
+        }
+
+        public void AddElement(string element)
+        {
+            dataCollection.Add(element);
+        }
+
+        public void RemoveElement(string element)
+        {
+            dataCollection.Remove(element);
+        }
+
+        public string GetElementByIndex(int index)
+        {
+            if (index >= 0 && index < dataCollection.Count)
+            {
+                return dataCollection[index];
+            }
+            return null;
+        }
+
+        public void DisplayData()
+        {
+            foreach (var item in dataCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void Dispose()
+        {
+            dataCollection.Clear();
+            dataCollection = null;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections.Generic;
+using System;
+namespace Assignment11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the data (comma-separated):");
+            string input = Console.ReadLine();
+            List<string> initialData = new List<string>(input.Split(','));
+
+            using (var largeDataCollection = new LargeDataCollection(initialData))
+            {
+                Console.WriteLine("Initial data in the collection:");
+                largeDataCollection.DisplayData();
+
+                Console.Write("Enter an element to add: ");
+                string elementToAdd = Console.ReadLine();
+                largeDataCollection.AddElement(elementToAdd);
+
+                Console.Write("Enter an element to remove: ");
+                string elementToRemove = Console.ReadLine();
+                largeDataCollection.RemoveElement(elementToRemove);
+
+                Console.WriteLine("Data in the collection after adding and removing:");
+                largeDataCollection.DisplayData();
+
+                Console.Write("Enter an index  number to access data: ");
+                if (int.TryParse(Console.ReadLine(), out int index))
+                {
+                    string accessedElement = largeDataCollection.GetElementByIndex(index);
+                    if (accessedElement != null)
+                    {
+                        Console.WriteLine($"Data at index {index}: {accessedElement}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index or data not found.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for index.");
+                }
+            } // The Dispose method will be called here automatically, freeing up resources.
+
+            // The largeDataCollection object is now disposed of and cannot be used.
+            Console.WriteLine("LargeDataCollection object has been disposed.");
+
+            Console.ReadKey();
+        }
+    }
+}
+
